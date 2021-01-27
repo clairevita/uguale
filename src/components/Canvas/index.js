cf021bcf192a5cbdc04f4751bfdec449480a3aa9
Claire Vita <clairej.vita@gmail.com>
2021-01-27 00:31:12 -0500

    unstylized canvas

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-
-function Canvas() {
-  return (
-  
-        <p>Canvas Element</p>
-   
-)}
+import {SketchField, Tools} from 'react-sketch';
+ 
+class Canvas extends React.Component {
+     render() {
+        return (
+            <SketchField width='600px' 
+                         height='600px' 
+                         tool={Tools.Pencil} 
+                         lineColor='black'
+                         lineWidth={3}/>
+        )
+     }
+}
+ 
 
 export default Canvas;
