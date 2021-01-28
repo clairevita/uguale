b6bbd290a52a18ee4e93a91d9728b55b1f983628
Claire Vita <clairej.vita@gmail.com>
2021-01-28 18:43:52 -0500

    canvas full funcitonality

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,19 +1,53 @@
 import React from "react";
-import {SketchField, Tools} from 'react-sketch';
-import './Canvas.css'; 
-     function Canvas() {
+import { ReactSketchCanvas } from "react-sketch-canvas";
+import './Canvas.css';
+import Button from "react-bootstrap/Button";
+const Canvas = class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.canvas = React.createRef();
+    }
+
+    render() {
         return (
             <div className='canvasContainer'>
-                            <SketchField width='500px' 
-                         height='500px' 
-                         tool={Tools.Pencil} 
-                         lineColor='black'
-                         lineWidth={3}/>
+                <ReactSketchCanvas
+                    ref={this.canvas}
+                    strokeWidth={5}
+                    strokeColor="black"
+                    width="100%"
+                    height="100%"
+
+                />
+                <Button
+                    onClick={() => {
+                        this.canvas.current.
+                            exportImage("png")
+                            .then(data => {
+                                console.log(data);
+                            })
+                            .catch(event => {
+                                console.log(event);
+                            });
+                    }}
+                >
+                    Submit
+              </Button>
+
+                <Button
+                    onClick={() => {
+                        this.canvas.current.
+                        resetCanvas()
+
+                    }}
+                >
+                    Erase
+              </Button>
+
             </div>
+        );
+    }
+};
 
-        )
-     }
-
- 
 
 export default Canvas;
