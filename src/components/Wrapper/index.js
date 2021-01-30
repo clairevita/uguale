ce755a5d4709c4a2baf244fcc5d140996414dc2a
Deirdre Cruice <dmcruice@gmail.com>
2021-01-30 11:36:49 -0500

    wrapper theme style

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+// import ConfigBttn from "../ConfigBttn/index";
 import "./style.css";
 
 function Wrapper(props) {
-  return <main className="wrapper" {...props} />;
+  const [themeState, setThemeState] = useState({
+    theme: "default",
+  });
+  
+  return <main className={themeState.theme} {...props} />;
 }
 
 export default Wrapper;
+
+
+
+
+    // <button onClick={() => setThemeState({ ...themeState, theme: "inverse" })} className="btn btn-info">
+    //   INVERSE COLORS
+    // </button>
