165be6f64dcaa94336b1e73024fe2ddfe289323b
Roman Mazzei <mazzeiroman@gmail.com>
2021-01-30 12:34:40 -0500

    routing for buttons is working

--- src/components/HomeBttn/index.js
+++ src/components/HomeBttn/index.js
@@ -3,9 +3,10 @@ import Button from "react-bootstrap/Button";
 import './HomeBttn.css';
 
 function HomeBttn() {
+
   return (
-  <Button bsPrefix= "HomeBttn">
-  home
+  <Button bsPrefix= "HomeBttn" onClick={event =>  window.location.href='/'}>
+   Home
  </Button>
 )}
 
