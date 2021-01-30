5cf852165067d18250a6e253d526014789ebddac
Logan Morro <68932638+loganfmorro@users.noreply.github.com>
2021-01-30 11:34:23 -0500

    Styling branch (#7)
    * got home and config buttons styled as well as page background
    
    * fixed button centering issue with Anthony's help

--- src/components/ConfigBttn/index.js
+++ src/components/ConfigBttn/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import './configBttn.css';
 
 function ConfigBttn() {
   return (
-  <Button variant="contained" color="secondary" className="ConfigBttn">
-  Config
+  <Button bsPrefix ="ConfigBttn">
+  config
  </Button>
 )}
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./footer.css";
 
 function Footer() {
   return (
