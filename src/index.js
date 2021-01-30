089cf92ad5d472614e974d3ed6210ad0cbbac16e
Deirdre Cruice <dmcruice@gmail.com>
2021-01-30 15:04:20 -0500

    abandoning the lazy and suspense approach

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { TYPE_OF_THEME } from './enum'; 
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-const DefaultTheme = React.lazy(() => import('./themes/defaulttheme'));
-const BerryTheme = React.lazy(() => import('./themes/berrytheme'));
-
-const ThemeSelector = ({ children }) => {
-    const CHOSEN_THEME = localStorage.getItem('TYPE_OF_THEME') || TYPE_OF_THEME.DEFAULT;
-    return (
-      <>
-        <React.Suspense fallback={<></>}>
-          {(CHOSEN_THEME === TYPE_OF_THEME.LIGHT_MODE) && <DefaultTheme />}
-          {(CHOSEN_THEME === TYPE_OF_THEME.DARK_MODE) && <BerryTheme />}
-        </React.Suspense>
-        {children}
-      </>
-    )
-  }
-
-ReactDOM.render(
-    <ThemeSelector>
-        <App/>
-    </ThemeSelector>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
