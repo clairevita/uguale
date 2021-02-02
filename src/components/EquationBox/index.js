cd2a31a458e3e92d4d8f3065eb824d179f0bbf08
Roman Mazzei <mazzeiroman@gmail.com>
2021-02-02 15:29:24 -0500

    bigger number will be on top in the equation

--- src/components/EquationBox/index.js
+++ src/components/EquationBox/index.js
@@ -8,30 +8,44 @@ class EquationBox extends React.Component {
     random2: 0
   };
 
+  componentDidMount() {
+    this.generateRandoms()
+  }
+
   // handleIncrement increments this.state.random1 by 1
   generateRandoms = () => {
     let difficulty = 2;
     let rand1 = Math.floor(Math.random() * (10 * difficulty));
     let rand2 = Math.floor(Math.random() * (10 * difficulty));
     // We always use the setState method to update a component's state
-    this.setState({ random1:  rand1,  random2:  rand2});
+    this.setState({ random1: rand1, random2: rand2 });
   };
 
   // The render method returns the JSX that should be rendered
   render() {
-    return (
+    if (this.state.random1 > this.state.random2) {
+      return (
         <div>
-      <div className="jumbotron jumbotron-fluid text-center ">
-        <div className="card-body">
-          <p className="card-text">{this.state.random1} + {this.state.random2}</p>
-        </div>
-      </div>
-       <button className="btn btn-primary" onClick={this.generateRandoms}>
-       Generate
+          <p className="card-text">{this.state.random1} + </p>
+      <p className="card-text">  {this.state.random2}</p>
+          <button className="btn btn-primary" onClick={this.generateRandoms}>
+            Generate
      </button>
-     </div>
-    );
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p className="card-text">{this.state.random2} + </p>
+      <p className="card-text"> {this.state.random1}</p>
+          <button className="btn btn-primary" onClick={this.generateRandoms}>
+            Generate
+     </button>
+        </div>
+      );
+    }
   }
 }
 
 export default EquationBox;
+ 
