ffe52c4714e503a9522385b3e0d2fa6ddbeda9eb
Claire Vita <clairej.vita@gmail.com>
2021-02-02 21:44:44 -0500

    equation box clean

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import Game from "./pages/Game";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/Home";
 import { MathProvider } from './utils/GlobalState';
+
 function App() {
   document.title = "Uguale";
   return (
     <div>
       <Router>
         <MathProvider>
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/game" component={Game} />
-        </Wrapper>
+          <Wrapper>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/game" component={Game} />
+          </Wrapper>
         </MathProvider>
       </Router>
     </div>
-  
+
   );
 }
 
--- src/components/EquationBox/index.js
+++ src/components/EquationBox/index.js
@@ -1,41 +1,24 @@
 import React from "react";
 import './EQBox.css';
-import { ReactSketchCanvas } from "react-sketch-canvas";
+import { useMathContext } from "../../utils/GlobalState"
 // By extending the React.Component class, Counter inherits functionality from it
-class EquationBox extends React.Component {
-    // Setting the initial state of the Counter component
-    state = {
-        random1: 0,
-        random2: 0
-    };
-
-  componentDidMount() {
-    this.generateRandoms()
-  }
+function EquationBox() {
+  // Setting the initial state of the Counter component
+  const [state, dispatch] = useMathContext();
 
   // handleIncrement increments this.state.random1 by 1
-  generateRandoms = () => {
-    let difficulty = 2;
-    let rand1 = Math.floor(Math.random() * (10 * difficulty));
-    let rand2 = Math.floor(Math.random() * (10 * difficulty));
-    // We always use the setState method to update a component's state
-    this.setState({ random1:  rand1,  random2:  rand2});
-  };
-    // The render method returns the JSX that should be rendered
-    render() {
-        return (
-            <div>
-                <div id="EQbox" className="jumbotron jumbotron-fluid text-center ">
-                    <div className="card-body">
-                        <p className="card-text">{this.state.random1} + {this.state.random2}</p>
-                    </div>
-                </div>
-                <button id="generateBtn" className="btn btn-primary" onClick={this.generateRandoms}>
-                    Generate
-                </button>
-            </div>
-        );
-    }
+  // The render method returns the JSX that should be rendered
+
+  return (
+    <div>
+      <div id="EQbox" className="jumbotron jumbotron-fluid text-center ">
+        <div className="card-body">
+          <p className="card-text">{this.state.random1} + {this.state.random2}</p>
+        </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default EquationBox;
