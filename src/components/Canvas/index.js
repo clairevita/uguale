243c5a7ca3015d00532b6765c5a78d818924bc2e
Roman Mazzei <mazzeiroman@gmail.com>
2021-02-02 20:49:04 -0500

    two line equations and canvas background

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import './Canvas.css';
 import Button from "react-bootstrap/Button";
+
 const Canvas = class extends React.Component {
     constructor(props) {
         super(props);
@@ -17,10 +18,14 @@ const Canvas = class extends React.Component {
                         strokeColor="black"
                         width="100%"
                         height="100%"
+                        background="url(https://cdn5.vectorstock.com/i/1000x1000/16/14/graph-paper-background-vector-3851614.jpg)"
                     />
                 </div>
                 <div className='buttons'>
-                <Button bsPrefix='submitBtn'>
+                <Button bsPrefix='submitBtn' onClick={() => {
+                    this.canvas.current.
+                    clearCanvas()
+                    }}>
                     Submit
                 </Button>
 
