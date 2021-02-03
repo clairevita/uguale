fe8ff928d2c6706972b0c4465916a7d179b2b116
Claire Vita <clairej.vita@gmail.com>
2021-02-02 20:41:43 -0500

    temporary field

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import './Canvas.css';
 import Button from "react-bootstrap/Button";
+import NumberField from '../NumberField';
+
 const Canvas = class extends React.Component {
     constructor(props) {
         super(props);
@@ -11,33 +13,43 @@ const Canvas = class extends React.Component {
         return (
             <div>
                 <div className='canvasContainer'>
-                <ReactSketchCanvas
+                    <ReactSketchCanvas
                         ref={this.canvas}
                         strokeWidth={5}
                         strokeColor="black"
                         width="100%"
                         height="100%"
-                    />
+                        className="ReactSketchCanvas"
+                    >
+
+                    </ReactSketchCanvas>
+
                 </div>
+                <NumberField />
                 <div className='buttons'>
-                <Button bsPrefix='submitBtn'>
-                    Submit
+                    <Button bsPrefix='submitBtn'
+                        onClick={() => {
+                            this.canvas.current.
+                                resetCanvas()
+                        }}
+                    >
+                        Submit
                 </Button>
 
-                <Button
-                    bsPrefix='eraseBtn'
-                    onClick={() => {
-                    this.canvas.current.
-                    resetCanvas()
-                    }}
-                >
-                    Erase
+                    <Button
+                        bsPrefix='eraseBtn'
+                        onClick={() => {
+                            this.canvas.current.
+                                resetCanvas()
+                        }}
+                    >
+                        Erase
                 </Button>
                 </div>
             </div>
         );
     }
-    
+
 };
 
 
--- src/components/NumberField/index.js
+++ src/components/NumberField/index.js
@@ -1,14 +1,15 @@
+import React from "react";
+import Form from 'react-bootstrap/Form';
 function NumberField() {
-
     return (
-      <div>
-      <form>
-        <input
-          type="text"
-        />
-      </form>
-      </div>
+        <div>
+            <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control size="lg" type="text"/>
+                </Form.Group>
+            </Form>
+        </div>
     )
-  }
-  
-  export default NumberField;
+}
+
+export default NumberField;
