1461ee29910aa382c14dea2675dceb2791e105cb
Claire Vita <clairej.vita@gmail.com>
2021-02-02 20:22:44 -0500

    creation of numberfield comp

---
+++ src/components/NumberField/index.js
@@ -0,0 +1,14 @@
+function NumberField() {
+
+    return (
+      <div>
+      <form>
+        <input
+          type="text"
+        />
+      </form>
+      </div>
+    )
+  }
+  
+  export default NumberField;
