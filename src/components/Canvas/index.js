14df6d0a5fd88a4b6d683567e07f522224cf28a4
Logan Morro <loganfmorro@gmail.com>
2021-02-02 21:33:55 -0500

    bringing mergies back

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -11,47 +11,12 @@ const Canvas = class extends React.Component {
         return (
             <div>
                 <div className='canvasContainer'>
-<<<<<<< HEAD
                     <ReactSketchCanvas
-=======
-                <ReactSketchCanvas
->>>>>>> 20ecb2626b9b43a4b60c2d39eef8f7f19f66b18b
                         ref={this.canvas}
                         strokeWidth={5}
                         strokeColor="black"
                         width="100%"
                         height="100%"
-<<<<<<< HEAD
-
-                    />
-                </div>
-                <div className='buttons'>
-                    <Button bsPrefix="submitBtn"
-                        onClick={() => {
-                            this.canvas.current.
-                                exportImage("png")
-                                .then(data => {
-                                    console.log(data);
-                                })
-                                .catch(event => {
-                                    console.log(event);
-                                });
-                        }}
-                    >
-                        submit
-                    </Button>
-
-                    <Button bsPrefix="eraseBtn"
-                        onClick={() => {
-                            this.canvas.current.
-                                resetCanvas()
-
-                        }}
-                    >
-                        erase
-                    </Button>
-
-=======
                     />
                 </div>
                 <div className='buttons'>
@@ -68,7 +33,6 @@ const Canvas = class extends React.Component {
                 >
                     Erase
                 </Button>
->>>>>>> 20ecb2626b9b43a4b60c2d39eef8f7f19f66b18b
                 </div>
             </div>
         );
