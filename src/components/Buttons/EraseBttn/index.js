4aac4319310c0d6a5d249db75816bb1e703e93ab
Claire Vita <clairej.vita@gmail.com>
2021-02-09 19:18:25 -0500

    button directories

---
+++ src/components/Buttons/EraseBttn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function EraseBttn(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default EraseBttn;
---
+++ src/components/Buttons/SkipBttn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SkipBttn(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default SkipBttn;
