9b495831c7513f5d49aafc4397565c7c7c717453
Claire Vita <clairej.vita@gmail.com>
2021-02-13 13:30:21 -0500

    database failure

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === "production") {
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync({ force: false }).then(function() {
+
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
