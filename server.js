ddd511baa2cbdc4304ac6e0b2b6382de644117fe
Claire Vita <clairej.vita@gmail.com>
2021-02-15 12:56:57 -0500

    port defined

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const db = require("./models");
 const routes = require("./routes");
 
@@ -10,8 +10,8 @@ const routes = require("./routes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
