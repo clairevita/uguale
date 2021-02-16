a37d28b66a0b3cd3cc27c8c9ffa7eb7c2b78e608
Claire Vita <clairej.vita@gmail.com>
2021-02-16 17:21:18 -0500

    error test 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,11 +26,11 @@ module.exports = {
       .catch(err => res.status(422).json(err)); 
   },
   update: function (req, res) {
-    console.log(req.params)
-    // db.User
-    //   .findOneAndUpdate({ _id: req.params.id }, req.body)
-    //   .then(dbModel => res.json(dbModel))
-    //   .catch(err => res.status(422).json(err));
+    console.log(req.params.id)
+    db.User
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
   findOne: function(req, res){
     db.User
