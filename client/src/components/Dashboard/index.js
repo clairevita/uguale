e20542a2d75d56d9b984949a8c6b8b027379f5c9
Deirdre Cruice <dmcruice@gmail.com>
2021-02-18 14:30:01 -0500

    dashboard index and css updates

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -9,17 +9,14 @@ function Dashboard() {
     const [state, dispatch] = useMathContext();
     return (
         <div>
-
-            <Jumbotron className={'text-center ' + state.night + state.themeStyle + 'dash jumbotron-fluid'}> 
-                <h1 class="display-4">PROFILE<span> USER.EMAIL?</span></h1>
-            </Jumbotron>
+            <h2 className={'text-center text-' + state.night + state.themeStyle + ' jumbotron-fluid display-4 profile'}>PROFILE<span> USER.EMAIL?</span></h2>
 
             <Row className="dash-row">
                 <Col Size="md-4">
-                    <Card bsPrefix={'border-left-' + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
+                    <Card bsPrefix={'border-left-' + state.night + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
                         <Card.Body className="left-padding">
                             <Row className="mini-dash-row">
-                                <div className={'text-' + state.themeStyle + ' font-weight-bold text-uppercase'}>User Since:</div>
+                                <div className={'text-' + state.night + state.themeStyle + ' font-weight-bold text-uppercase'}>User Since:</div>
                             </Row>
                             <Row className="mini-dash-row">
                                 <div className="h5 font-weight-bold text-secondary text-uppercase">Feb 2, 2020</div>
@@ -28,10 +25,10 @@ function Dashboard() {
                     </Card>
                 </Col>
                 <Col Size="md-4">
-                    <Card bsPrefix={'border-left-' + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
+                    <Card bsPrefix={'border-left-' + state.night + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
                         <Card.Body className="left-padding">
                             <Row className="mini-dash-row">
-                                <div className={'text-' + state.themeStyle + ' font-weight-bold text-uppercase'}># of Days Practiced:</div>
+                                <div className={'text-' + state.night + state.themeStyle + ' font-weight-bold text-uppercase'}># of Days Practiced:</div>
                             </Row>
                             <Row className="mini-dash-row">
                                 <div className="h5 font-weight-bold text-secondary text-uppercase"><span className="average-price">17</span></div>
@@ -40,10 +37,10 @@ function Dashboard() {
                     </Card>
                 </Col>
                 <Col Size="md-4">
-                    <Card bsPrefix={'border-left-' + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
+                    <Card bsPrefix={'border-left-' + state.night + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
                         <Card.Body className="left-padding">
                             <Row className="mini-dash-row">
-                                <div className={'text-' + state.themeStyle + ' font-weight-bold text-uppercase'}>Completion Rate:</div>
+                                <div className={'text-' + state.night + state.themeStyle + ' font-weight-bold text-uppercase'}>Completion Rate:</div>
                             </Row>
                             <Row className="mini-dash-row">
                                 <div className="h5 font-weight-bold text-secondary text-uppercase"><span className="completion-rate">90%</span></div>
@@ -54,9 +51,9 @@ function Dashboard() {
             </Row>
             <Row className="dash-row">
                 <Col Size="md-12">
-                    <Card bsPrefix={'border-left-' + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
+                    <Card bsPrefix={'border-left-' + state.night + state.themeStyle + ' shadow p-3 mb-5 bg-white rounded'}>
                         <Card.Header>
-                            <div className={'h4 font-weight-bold text-uppercase text-' + state.themeStyle}> Give these another try:
+                            <div className={'h4 font-weight-bold text-uppercase text-' + state.night + state.themeStyle}> Give these another try:
                             </div>
                         </Card.Header>
                         <Card.Body className="left-padding">
@@ -84,5 +81,8 @@ export default Dashboard;
 
 
 
+            // {/* <Jumbotron className={'text-center ' + state.night + state.themeStyle + 'dash jumbotron-fluid'}> 
+            //     <h2 className="display-4 profile">PROFILE<span> USER.EMAIL?</span></h2>
+            // </Jumbotron> */}
 // className={state.night + state.themeStyle + 'main'}
 // {/* <hr class="my-4"></hr> */}
